a01acbec017e40c6db5e437f3f5aaa6517871e74
zaxon02 <zaxon02@gmail.com>
2021-01-05 19:18:23 +0300

    added surfaces

--- kate.py
+++ kate.py
@@ -1,15 +1,30 @@
 import pygame, sys
 
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos,  450))
+    screen.blit(floor_surface, (floor_x_pos + 288, 450))
+
 pygame.init()
-screen = pygame.display.set_mode((576, 1024))
+screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()
 
-bg_surface = pygame.image.load('assets/background')
+bg_surface = pygame.image.load('assets/background-day.png').convert()
+
+floor_surface = pygame.image.load('assets/base.png').convert()
+floor_x_pos = 0
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.blit(bg_surface, (0, 0))
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -288:
+        floor_x_pos = 0
+    screen.blit(floor_surface, (floor_x_pos, 450))
 
     pygame.display.update()
     clock.tick(120)
