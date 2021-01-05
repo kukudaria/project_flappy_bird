59b897fcadbdcfd6ab66629b052e9f4ab49904be
zaxon02 <zaxon02@gmail.com>
2021-01-05 18:51:20 +0300

    added images

--- kate.py
+++ kate.py
@@ -1,1 +1,15 @@
-print()
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((576, 1024))
+clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load('assets/background')
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(120)
