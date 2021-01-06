2894d8c7804d6b4c398937a5b2aa9e8b56ed9ccc
daria <kukudaria@gmail.com>
2021-01-06 19:17:52 +0300

    bird animation and score

--- kate.py
+++ kate.py
@@ -52,10 +52,25 @@ def bird_animation():
     return new_bird, new_bird_rect
 
 
-def score_display():
-    score_surface = game_font.render('Score', True, (255, 255, 255))
-    score_rect = score_surface.get_rect(center=(144, 50))
-    screen.blit(score_surface, score_rect)
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(144, 50))
+        screen.blit(score_surface, score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(144, 50))
+        screen.blit(score_surface, score_rect)
+
+        high_score_surface = game_font.render(f'High score: {int(high_score)}', True, (255, 255, 255))
+        high_score_rect = high_score_surface.get_rect(center=(144, 425))
+        screen.blit(high_score_surface, high_score_rect)
+
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
 
 
 pygame.init()
@@ -110,6 +125,7 @@ while True:
                 pipe_list.clear()
                 bird_rect.center = (50, 256)
                 bird_movement = 0
+                score = 0
 
         if event.type == SPAWNPINE:
             pipe_list.extend(create_pipe())
@@ -137,7 +153,11 @@ while True:
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
 
-        score_display()
+        score += 0.02
+        score_display('main_game')
+    else:
+        high_score = update_score(score, high_score)
+        score_display('game_over')
 
     # Floor
     floor_x_pos -= 1
